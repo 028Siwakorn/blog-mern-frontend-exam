79d660017e52f109731ceecab46da43bdf9bda13
028Siwakorn <664259028@webmail.npru.ac.th>
2026-02-09 14:28:27 +0700

    gh-action

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(),tailwindcss()],
+  base: "/blog-mern-frontend-exam/",
+  plugins: [react(), tailwindcss()],
 });
